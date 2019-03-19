811c3f0ea84080005c049537c8d28952ea237700
Jacob Krucinski <jacob1576@gmail.com>
2019-03-19 19:42:58 -0400

    Ran calibration script with Priyanshu's sample images!

--- FRC_2019_Vision_Calib/Avg_Calib_Data.m
+++ FRC_2019_Vision_Calib/Avg_Calib_Data.m
@@ -2,28 +2,54 @@ function [calibOutput] = Avg_Calib_Data(Calib)
 
 nCalibs = size(Calib, 2);
 
-H_min_sum = 0;
-H_max_sum = 0;
-S_min_sum = 0;
-S_max_sum = 0;
-V_min_sum = 0;
-V_max_sum = 0;
+H_min_min = 999;
+H_max_max = -999;
+S_min_min = 999;
+S_max_max = -999;
+V_min_min = 999;
+V_max_max = -999;
 
 for i=1:nCalibs
-    H_min_sum = H_min_sum + Calib(i).H_min;
-    H_max_sum = H_max_sum + Calib(i).H_max;
-    S_min_sum = S_min_sum + Calib(i).S_min;
-    S_max_sum = S_max_sum + Calib(i).S_max;
-    V_min_sum = V_min_sum + Calib(i).V_min;
-    V_max_sum = V_max_sum + Calib(i).V_max;
+    H_min = Calib(i).H_min;
+    H_max = Calib(i).H_max;
+    S_min = Calib(i).S_min;
+    S_max = Calib(i).S_max;
+    V_min = Calib(i).V_min;
+    V_max = Calib(i).V_max;
+    
+    if H_min < H_min_min
+        H_min_min = H_min;
+    end
+    
+    if H_max > H_max_max
+        H_max_max = H_max;
+    end
+    
+    if S_min < S_min_min
+        S_min_min = S_min;
+    end
+    
+    if S_max > S_max_max
+        S_max_max = S_max;
+    end
+    
+    if V_min < V_min_min
+        V_min_min = V_min;
+    end
+    
+    if V_max > V_max_max
+        V_max_max = V_max;
+    end
+    
+    
 end
 
-avgCalib.H_min = H_min_sum / nCalibs;
-avgCalib.H_max = H_max_sum / nCalibs;
-avgCalib.S_min = S_min_sum / nCalibs;
-avgCalib.S_max = S_max_sum / nCalibs;
-avgCalib.V_min = V_min_sum / nCalibs;
-avgCalib.V_min = V_max_sum / nCalibs;
+avgCalib.H_min = H_min_min;
+avgCalib.H_max = H_max_max;
+avgCalib.S_min = S_min_min;
+avgCalib.S_max = S_max_max;
+avgCalib.V_min = V_min_min;
+avgCalib.V_max = V_max_max;
 
 calibOutput = avgCalib;
 end
--- FRC_2019_Vision_Calib/Calibrate_Vision_Target_v005.m
+++ FRC_2019_Vision_Calib/Calibrate_Vision_Target_v005.m
@@ -12,8 +12,8 @@ startImg = input('Start image ID\n');
 
 %% Load in all images first
 if target_type == 2
-    for i=startImg:numImgs
-        img_path = strcat('Calibration_Images_2019_03_16/00', num2str(i), '.PNG');
+    for i=startImg:2:numImgs
+        img_path = strcat('Calibration_Images_2019_03_19/', num2str(i), '.jpg');
         img = imread(img_path);
 
         % Rotate the image so that vision targets are straight up and down
@@ -26,29 +26,29 @@ if target_type == 2
         rectL = ginput(2);
         roundedL = round(rectL);
 
-        disp('Click in the upper left and then the lower right of the reflective tape...');
-        rectR = ginput(2);
-        roundedR = round(rectR);
+%         disp('Click in the upper left and then the lower right of the reflective tape...');
+%         rectR = ginput(2);
+%         roundedR = round(rectR);
 
         % Get target subsections from ginput
         target_L_img = img( roundedL(1,2):roundedL(2,2) , roundedL(1,1):roundedL(2,1), : ); % x then y
-        target_R_img = img( roundedR(1,2):roundedR(2,2) , roundedR(1,1):roundedR(2,1), : ); % x then y
+%         target_R_img = img( roundedR(1,2):roundedR(2,2) , roundedR(1,1):roundedR(2,1), : ); % x then y
 
         % Show target subsections in separate windows
         figure('Name', img_path, 'NumberTitle', 'off'); hold on;
         imshow(target_L_img);
-        figure('Name', img_path, 'NumberTitle', 'off'); hold on;
-        imshow(target_R_img);
+%         figure('Name', img_path, 'NumberTitle', 'off'); hold on;
+%         imshow(target_R_img);
         
         % Find Calib values
         % Use i in order to have all images as their own structs of data
         index = (i - startImg) + 1;
-        Calib(index) = Find_Calib_Parameters_v001(target_L_img, target_R_img);
+        Calib(index) = Find_Calib_Parameters_v001(target_L_img, target_L_img);
     end
 
 else
-    for i=startImg:numImgs
-        img_path = strcat('Calibration_Images_2019_03_16/00', num2str(i), '.PNG');
+    for i=startImg:2:numImgs
+        img_path = strcat('Calibration_Images_2019_03_19/', num2str(i), '.jpg');
         img = imread(img_path);
         figure('Name', img_path, 'NumberTitle', 'off')
         imshow(img);
@@ -73,14 +73,14 @@ else
     end
 end
 
-%% Take the average of Calib parameters from each image
+%% Take the min/max of Calib parameters from each image
 avgCalib = Avg_Calib_Data(Calib)
 
 %% Now write .h file
 if target_type==1
-    Calibration_Data_Make_h_File_v002(Calib, 'FloorLines_Calibration.h');
+    Calibration_Data_Make_h_File_v002(avgCalib, 'FloorLines_Calibration.h');
 else % target_type == 2
-    Calibration_Data_Make_h_File_v002(Calib, 'VisionTarget_Calibration.h');
+    Calibration_Data_Make_h_File_v002(avgCalib, 'VisionTarget_Calibration.h');
 end
 
 %% Display that code is done running
